66b4d684da2ae508b155f359973fb7f77ba2cd6c
Vianney <vianney.guesdon.dev@gmail.com>
2019-04-23 20:10:13 +0200

    corrections

--- src/App.js
+++ src/App.js
@@ -10,13 +10,21 @@ class App extends React.Component {
       color: "red",
       name: ""
     }
+    // Toujours binder le this
     this.onClickColor=this.onClickColor.bind(this);
+    this.onChangeName=this.onChangeName.bind(this);
   }
 
   onClickColor() {
     this.setState({
       color: this.states.color === "blue" ? "red" : "blue"
-    })
+    });
+  }
+
+  onChangeName(name) {
+    this.setState({
+      name
+    });
   }
 
   render() {
@@ -26,6 +34,7 @@ class App extends React.Component {
           name={this.state.name} 
           color={this.state.color} 
           onClick={this.onClickColor}
+          onChange={this.onChangeName}
         />
       </div>
     )
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,21 +4,16 @@ class Form extends React.Component {
 
   render() {
     console.log('Form#render this props', this.props);
-    console.log('Form#render this state', this.state);
+    // console.log('Form#render this state', this.state);
     return(
       <div style={{
         backgroundColor: this.props.color
       }}>
         <input 
-          type="text" 
+          type="text"
           value={this.props.name}
           onChange={(evt) => {
-            const name = evt.target.value;
-            console.log("name", name)
-            // Je change le state (permet de récuperer la totalité du champ)
-            this.setState({
-              name
-            });
+            this.props.onChange(evt.target.value);
           }}
         />
         <button onClick={this.props.onClick}>Click</button>
